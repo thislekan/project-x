4f55f14ab8b564213fa70b701c8d540712acb5b8
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2021-01-03 00:12:52 +0100

    chore(fix-netlify-build): troubleshooting
    - fix build
    
    [Delivers #176331556]

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,66 +1,65 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+
+const { NODE_ENV } = process.env;
 
 module.exports = {
-  mode: 'development',
-  entry: './src/index.tsx',
-  devtool: 'inline-source-map',
-  context: path.resolve(__dirname, ''),
+  mode: "development",
+  entry: "./src/index.tsx",
+  devtool: "inline-source-map",
+  context: path.resolve(__dirname, ""),
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: './index.html',
-      title: 'project-x',
+      template: "./public/index.html",
+      filename: "./index.html",
+      title: "project-x",
     }),
-    new BundleAnalyzerPlugin(),
+    NODE_ENV !== "production" && new BundleAnalyzerPlugin(),
   ],
   resolve: {
-    extensions: ['.tsx', '.jsx', '.ts', '.js', '.json', '.css', '.scss'],
+    extensions: [".tsx", ".jsx", ".ts", ".js", ".json", ".css", ".scss"],
   },
   module: {
     rules: [
       {
-        use: ['babel-loader', 'eslint-loader'],
+        use: ["babel-loader", "eslint-loader"],
         test: /\.js$/,
         exclude: /node_modules/,
       },
       {
-        use: ['babel-loader', 'eslint-loader'],
+        use: ["babel-loader", "eslint-loader"],
         test: /\.jsx$/,
       },
       {
         test: /\.tsx?$/,
-        use: ['ts-loader'],
+        use: ["ts-loader"],
         exclude: /node_modules/,
       },
       {
         test: /\.s[ac]ss$/i,
         use: [
-          process.env.NODE_ENV !== 'production'
-            ? 'style-loader'
-            : MiniCssExtractPlugin.loader,
-          'css-loader',
-          'sass-loader',
-
+          process.env.NODE_ENV !== "production" ? "style-loader" : MiniCssExtractPlugin.loader,
+          "css-loader",
+          "sass-loader",
         ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: 'asset/resource',
+        type: "asset/resource",
       },
     ],
   },
   optimization: {
     splitChunks: {
-      chunks: 'all',
+      chunks: "all",
       // minSize: 10,
       // minRemainingSize: 0,
       // maxSize: 20000,
@@ -90,17 +89,17 @@ module.exports = {
     },
   },
   devServer: {
-    contentBase: path.resolve(__dirname, 'dist'),
+    contentBase: path.resolve(__dirname, "dist"),
     compress: true,
     port: 9000,
     open: true,
     historyApiFallback: true,
   },
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, "dist"),
     // filename: '[name].bundle.js',
-    filename: '[name].bundle.js',
-    chunkFilename: '[name].bundle.js',
-    publicPath: '/',
+    filename: "[name].bundle.js",
+    chunkFilename: "[name].bundle.js",
+    publicPath: "/",
   },
 };
