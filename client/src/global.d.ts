8c44af5600be82863c97a048b1136ed9f516fd47
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2021-01-16 23:43:04 +0100

    chore(client-to-endpoint): connect to backend
    - declaration and types
    - add images
    
    [Starts #176473158]

---
+++ client/src/global.d.ts
@@ -0,0 +1,19 @@
+declare module "*.gif" {
+  export default "" as string;
+}
+
+declare module "*.png" {
+  export default "" as string;
+}
+
+declare module "*.svg" {
+  export default "" as string;
+}
+
+declare module "*.jpg" {
+  export default "" as string;
+}
+
+declare module "*.jpeg" {
+  export default "" as string;
+}
