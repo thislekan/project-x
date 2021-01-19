5ed53ae8dec11ceb616438ded93b7e997d6f8567
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2021-01-19 03:27:58 +0100

    feat(add-socket-to-server): install socket
    - install socket.io
    - add socket setup
    
    [Begins #176532142]

--- server/src/index.js
+++ server/src/index.js
@@ -3,15 +3,22 @@ import "regenerator-runtime/runtime";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import http from "http";
+import socketIO from "socket.io";
 
 import config from "config";
 import routes from "routes";
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIO(server);
 
 app.use(cors());
 app.use(cookieParser());
 app.use(routes);
+
+const emitSomething = (socket) =>
+  socket.emit("fromAPI", "Emitting something back to client");
 
 app.get("/", (_req, res) =>
   res.status(200).json({
@@ -19,6 +26,19 @@ app.get("/", (_req, res) =>
   })
 );
 
-app.listen(config.PORT, () =>
+io.on("connection", (socket) => {
+  console.log("New client just connected");
+
+  socket.on("emitSomething", (data) => {
+    console.log(`emitted something (like: ${data}) to the backend`);
+    emitSomething(socket);
+  });
+
+  socket.on("disconnect", () => {
+    console.log("Client is disconnected");
+  });
+});
+
+server.listen(config.PORT, () =>
   console.log(`App is now live on port ${config.PORT}`)
 );
