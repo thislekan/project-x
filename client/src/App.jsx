d431378f9dde4ce09ec527c47933082b0d96429b
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2021-01-30 14:01:08 +0100

    chore(client-socket-connection): change string
    - change connection string
    
    [Delivers #176729020]

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,13 @@ import { useEffect } from "react";
 import socketIOClient from "socket.io-client";
 import Routes from "./routes";
 
-const ENDPOINT = "http://127.0.0.1:8085/";
+const { REACT_APP_BACKEND_URL } = process.env;
 const App = () => {
   useEffect(() => {
-    console.log("shebi it got here");
-    const socket = socketIOClient(ENDPOINT);
-
+    const socket = socketIOClient(REACT_APP_BACKEND_URL);
     socket.emit("emitSomething", "FUCK THIS");
     socket.on("fromAPI", (data) => console.log({ data }));
+
     return () => {
       socket.disconnect();
     };
