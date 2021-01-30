a6e327a92b9cb8ba89a691c940cb5c5ac3a5e3d0
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2021-01-30 13:14:19 +0100

    bug(client-socket-handshake): resolve bug
    - change scripts to prevent build failure
    
    [Delivers #176651440]

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,25 @@
+import { useEffect } from "react";
+import socketIOClient from "socket.io-client";
 import Routes from "./routes";
 
-const App = () => (
-  <div className="App">
-    <Routes />
-  </div>
-);
+const ENDPOINT = "http://127.0.0.1:8085/";
+const App = () => {
+  useEffect(() => {
+    console.log("shebi it got here");
+    const socket = socketIOClient(ENDPOINT);
+
+    socket.emit("emitSomething", "FUCK THIS");
+    socket.on("fromAPI", (data) => console.log({ data }));
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <Routes />
+    </div>
+  );
+};
 
 export default App;
