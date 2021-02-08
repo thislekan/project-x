50247baced89d80d1aa5d50c219954e2809b00d5
Lekan Omoniyi <ibrahimlekanomoniyi@gmail.com>
2021-02-09 00:22:08 +0100

    chore(build-user-dashboard): WIP
    - abandon task for more important task
    
    [Starts #176729213]

--- client/src/components/molecules/navBar/sideBar/index.jsx
+++ client/src/components/molecules/navBar/sideBar/index.jsx
@@ -1,3 +1,28 @@
-const SideBar = () => <p>Side bar here!!!</p>;
+import { SimpleButton } from "../../../atoms/buttons";
+import { FlexColumn } from "../../../atoms/wrappers";
+
+const SideBar = () => {
+  return (
+    <div className="sidebar">
+      <FlexColumn addClass="sidebar__wrapper">
+        <div className="sidebar__wrapper__anchor-wrapper">
+          <SimpleButton content="MUSIC" />
+          <div className="options-tray">
+            <SimpleButton content="ARTIST" />
+            <SimpleButton content="ALBUM" />
+            <SimpleButton content="GENRE" />
+          </div>
+        </div>
+        <div className="sidebar__wrapper__anchor-wrapper">
+          <SimpleButton content="ANALYTICS" />
+          <div className="options-tray">
+            <SimpleButton content="TOP ARTISTs" />
+            <SimpleButton content="TOP SONGS" />
+          </div>
+        </div>
+      </FlexColumn>
+    </div>
+  );
+};
 
 export default SideBar;
